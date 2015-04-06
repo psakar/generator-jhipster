afcdf943b16e89eb98bd518accf7b382c6a96205
saki <saki@chare.eu>
2015-04-06 14:59:27 +0200

    fix global leak (idx)

--- entity/templates/src/main/webapp/app/_entity-detail-controller.js
+++ entity/templates/src/main/webapp/app/_entity-detail-controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('<%=angularAppName%>')
-    .controller('<%= entityClass %>DetailController', function ($scope, $rootScope, $stateParams, entity<% for (idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %>) {
+    .controller('<%= entityClass %>DetailController', function ($scope, $rootScope, $stateParams, entity<% for (var idx in differentTypes) { %>, <%= differentTypes[idx] %><% } %>) {
         $scope.<%= entityInstance %> = entity;
         $scope.load = function (id) {
             <%= entityClass %>.get({id: id}, function(result) {
